68f22a9750c10a5ce239cb659a492230bf804929
ZaharDomolego <domolegozahar@gmail.com>
2023-11-29 11:18:29 +0800

    Added auth mechanism

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
 import { Container, useTheme } from '@mui/material'
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 
 import ContentWrap from './content/contentWrap'
 import { routes } from './routes'
 import NotFound from './notFound/NotFound'
 import Auth from './auth/Auth'
+import useApi from '../hooks/useApi'
+import {changeUser} from '../redux/actions'
 
 export default function App() {
+    const location = useLocation()
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const userData = useSelector()
+    const callApi = useApi()
     const theme = useTheme()
+
+    useEffect(() => {
+        callApi('/users/account', 'GET', null, null).then(responseData => {
+            if (responseData.status == 200) {
+                if (responseData.data !== userData) {
+                    dispatch(changeUser(responseData.data))
+                }
+            }
+            else {
+                if (location.pathname !== routes.auth) {
+                    dispatch(changeUser(null))
+                    navigate(routes.auth)
+                }
+            }
+        })
+    }, [location.pathname])
 
     return (
         <Container maxWidth={false} disableGutters sx={{
